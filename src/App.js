b42cc1f458338102b12cbbf6158bc2d974d9d85b
William Eng <wneng@utexas.edu>
2023-03-29 00:09:07 -0500

    Fix App file structure and create modular header component

--- src/App.js
+++ src/App.js
@@ -1,36 +1,40 @@
-// import logo from './logo.svg';
-import './App.css';
-import React, {useState} from 'react'
+import React from 'react';
+import { Container } from 'react-bootstrap';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-import Hamburger from './Hamburger';
-import SideMenu from './SideMenu';
+import SignUp from './components/SignUp';
+import RecipeBook from './components/RecipeBook';
+import Profile from './components/Profile';
+import MainMenu from './components/MainMenu';
+import LogIn from './components/LogIn';
+import ProtectedRoute from './components/ProtectedRoute';
+import { AuthProvider } from './contexts/AuthContext';
 
 function App() {
-  const [isMenuActive, activeMenu] = useState(false)
+    return (
 
-  return (
- 
-    <div className="App">
-      <header className="App-header">
-
-        {/* Hamburger Menu */}
-        <div className='icon'>
-          <Hamburger 
-            fill="#fff" 
-            onMenuClick={() => activeMenu(!isMenuActive)} 
-          />
-        </div>
-
-        {/* Menu */}
-        <SideMenu 
-          isMenuActive={isMenuActive}
-          onOverLayClick = {() => activeMenu(!isMenuActive)}/>
-
-      </header>
-    </div>
-
+      <Container
+      className="d-flex align-items-center justify-content-center"
+      style={{ minHeight: "100vh" }}
+    >
+      <div className="w-100" style={{ maxWidth: "400px" }}>
+        
+        <BrowserRouter>
+          <AuthProvider>
+            <Routes>
+              <Route exact path="/" element={<ProtectedRoute><MainMenu/></ProtectedRoute>} />
+              <Route path="/signup" element={<SignUp/>} />
+              <Route path="/login" element={<LogIn/>} />
+              <Route path="/recipebook" element={<RecipeBook/>} />
+              <Route path="/profile" element={<Profile/>} />
+            </Routes>
+          </AuthProvider>
+        </BrowserRouter>
+      </div>
+    </Container>
+  )
     
-  );
+  
+  
 }
-
-export default App;
+export default App
