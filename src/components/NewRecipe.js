93e2aafd0c4896af188d60b8fc59c8484debf44a
danthynguyen <danthynguyen@gmail.com>
2023-03-29 19:29:02 -0500

    adding header buttons

--- src/components/NewRecipe.js
+++ src/components/NewRecipe.js
@@ -1,9 +1,51 @@
 import React from 'react'
+import '../App.css';
 
 export default function NewRecipe() {
     return (
         <div>
-            putting something here
+            {stepsHeader()}
         </div>
     )
+}
+
+function stepsHeader(){
+
+    return(
+        <div class = "steps-header">
+            <button class = 'stepsButton'>
+                1
+            </button>
+            <button class = 'stepsButton'>
+                2
+            </button>
+            <button class = 'stepsButton'>
+                3
+            </button>
+            <button class = 'stepsButton'>
+                4
+            </button>
+        </div>
+    )
+}
+
+function Import(){
+    return(
+        <div class = "import">
+            <form>
+                <input type = "text">
+                    
+                </input>
+            </form>
+        </div>
+    )
+}
+function Steps(){
+
+}
+function Ingrediants(){
+
+}
+function Finalize(){
+
 }
