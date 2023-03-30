fd369b9014546fb8682bef6d7dfb1146b88220b3
jcderry5 <jcderry5@gmail.com>
2023-03-30 16:21:59 -0500

    Account for ingredients that cannot be substituted

--- src/components/IngredientSubstitute.js
+++ src/components/IngredientSubstitute.js
@@ -2,9 +2,9 @@ import React, { useRef, useState } from 'react'
 
 
 const getIngredientData = function(ingredientName) {
-	let ingredientToSub = encodeURI(ingredientName.ingredient_a)
+	let encodedIngredient = encodeURI(ingredientName.ingredient_a)
 	// let URL = 'https://api.spoonacular.com/food/ingredients/substitutes?apiKey=14642766b5284fb08af1d8eb4aa030ac&ingredientName=butter'
-	let URL = 'https://api.spoonacular.com/food/ingredients/substitutes?apiKey=14642766b5284fb08af1d8eb4aa030ac&ingredientName=' + ingredientToSub
+	let URL = 'https://api.spoonacular.com/food/ingredients/substitutes?apiKey=14642766b5284fb08af1d8eb4aa030ac&ingredientName=' + encodedIngredient
 
 	alert("You are about to make a fetch to the API")
 
@@ -12,10 +12,20 @@ const getIngredientData = function(ingredientName) {
 	fetch(URL)
 		.then(response => response.json())
 		.then(response => {
-			for (const currSub of response.substitutes) {
-				result += (currSub + '\n')
+			if (response.status === 'failure') {
+				console.log("ERROR!!!")
+				console.log(response)
+				console.log(ingredientName.ingredient_a)
+				console.log(encodedIngredient)
+				alert("This ingredient cannot be substituted")
+			} else {
+				console.log(response)
+				console.log(response.substitutes)
+				for (const currSub of response.substitutes) {
+					result += (currSub + '\n')
+				}
+				alert(result)
 			}
-			alert(result)
 		})
 }
 
