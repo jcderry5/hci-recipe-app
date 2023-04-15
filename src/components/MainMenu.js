ecc77e7e16d532ca61c61c03559ee5257d131d3f
danthynguyen <danthynguyen@gmail.com>
2023-04-14 22:56:26 -0500

    stashing

--- src/components/MainMenu.js
+++ src/components/MainMenu.js
@@ -2,6 +2,7 @@ import React, { useState } from "react"
 import { Card, Button, Alert } from "react-bootstrap"
 import { useAuth } from "../contexts/AuthContext"
 import { Link, useNavigate} from "react-router-dom"
+import '../RecipeResults.css'
 
 export default function MainMenu() {
   const { user, logout } = useAuth();
@@ -20,14 +21,16 @@ export default function MainMenu() {
     <> 
       {<Card>
         <Card.Body>
-        <div className="row">
-        <div class="col-6" style={{display: 'flex', justifyContent: 'left'}}><b>Menu</b> </div> </div>
-        <div className="row">
-        <div class="col-6" style={{display: 'flex', justifyContent: 'center'}}><b>Recipe Remix</b> </div> </div>
-        <div className="row">
-        <div class="col-6" style={{display: 'flex', justifyContent: 'center'}}><b>Welcome!</b> </div> </div>    
-        <div className="row">
-        <div class="col-6" style={{display: 'flex', justifyContent: 'center'}}><b>Logo Will Go Here</b> </div> </div>    
+          <div class = "main-menu-card">
+            <div className="row">
+            <div class="col" style={{display: 'flex', justifyContent: 'left'}}><b>Main Menu</b> </div> </div>
+            <div className="row">
+            <div class="col" style={{display: 'flex', justifyContent: 'center'}}><b>Recipe Remix</b> </div> </div>
+            <div className="row">
+            <div class="col" style={{display: 'flex', justifyContent: 'center'}}><b>Welcome!</b> </div> </div>    
+            <div className="row">
+            <div class="col" style={{display: 'flex', justifyContent: 'center'}}><b>Logo Will Go Here</b> </div> </div>    
+          </div>
         </Card.Body>
       </Card>}
       <div className="w-100 text-center mt-2" style={{ display: 'flex', justifyContent: 'center' }}>
@@ -36,9 +39,12 @@ export default function MainMenu() {
             <div className="w-100 text-center mt-2" style={{display: 'flex', justifyContent: 'center'}}>
                 <Link to="/hci-recipe-app/profile">Update Profile</Link>
             </div>
-            <button onClick={handleLogout} className='border px-6 py-2 my-4' style={{display: 'flex', justifyContent: 'center'}}>
-        Logout
-      </button>
+      <div className="row" style={{display: 'flex', justifyContent: 'center'}}>
+        <button onClick={handleLogout} className='border px-6 py-2 my-4' style={{display: 'flex', justifyContent: 'center', width: '50%'}}>
+          Logout
+        </button>
+      </div>
+      
     </>
   )
 }
